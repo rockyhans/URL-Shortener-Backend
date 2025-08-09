896943d4fc001e55df852e4e86b13f1f58c305d5
rockyhans <rdanishrizwan@gmail.com>
2025-08-09 19:58:20 +0530

    Finished

--- index.js
+++ index.js
@@ -10,7 +10,27 @@ import { fileURLToPath } from "url";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+const allowedOrigins = [
+  "https://url-shortener-frontend-chi-seven.vercel.app/",
+  "http://localhost:5174",
+  "http://localhost:5173",
+  "http://localhost:1001",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
@@ -82,7 +102,6 @@ app.get(/^\/([a-zA-Z0-9_-]{5,15})$/, async (req, res) => {
 
   return res.redirect(urlDoc.longUrl);
 });
-
 
 // Admin middleware (simple header-based auth for demo)
 const adminAuth = (req, res, next) => {
